e930eda7befa65bb1f2e9ae38791f259957d6a50
jbmuir <jack.muir@earth.ox.ac.uk>
2023-11-01 09:17:28 +0000

    in principle working

--- examples/paper_main_2D.py
+++ examples/paper_main_2D.py
@@ -4,6 +4,8 @@ import jax.numpy as jnp
 import jax
 import optax
 from jax.tree_util import Partial
+import matplotlib.pyplot as plt
+import pickle
 
 
 sys.path.insert(0, "../terrapinn")
@@ -16,6 +18,8 @@ from training import *
 
 # generate test data and variables for this experiment
 regenerate_data = False
+retrain_radial = False
+retrain_full = True
 sys.path.insert(1, "../seismic-cpml")
 from seismic_CPML_2D_pressure_second_order import seismicCPML2D
 
@@ -97,77 +101,157 @@ if regenerate_data:
     np.save("data/c0_velocity.npy", c0_array[::10,::10])
     np.save("data/cg_wavefield.npy", cg_wavefield[::10,::10,::10])
     np.save("data/cg_velocity.npy", cg_array[::10,::10])
-
+    np.save("data/c0_wavefield_line.npy", c0_wavefield[:,xv==0,yv>=0])
 else:
     print("Not Regenerating Data")
 
 # recalculate dx, dy etc. as we have decimated the wavefields after calculation to save space
-dx = dx*10
-dy = dy*10
-dt = dx/ (4*np.sqrt(2)*c0)# target fine sampled deltas
-nx = int(np.floor((x_lim[1]-x_lim[0])/dx))+1
-ny = int(np.floor((y_lim[1]-y_lim[0])/dy))+1
-nt = int(np.floor((t_lim[1]-t_lim[0])/dt))+1
-X, xv, yv = generate_x(x_lim, y_lim, nx, ny)
-tv = np.linspace(t_lim[0], t_lim[1], nt)
+# dx = dx*10
+# dy = dy*10
+# dt = dx/ (4*np.sqrt(2)*c0)# target fine sampled deltas
+# nx = int(np.floor((x_lim[1]-x_lim[0])/dx))+1
+# ny = int(np.floor((y_lim[1]-y_lim[0])/dy))+1
+# nt = int(np.floor((t_lim[1]-t_lim[0])/dt))+1
+# X, xv, yv = generate_x(x_lim, y_lim, nx, ny)
+# tv = np.linspace(t_lim[0], t_lim[1], nt)
 
 #radial model training
 
-const_model_radial_data = jnp.load("data/c0_wavefield.npy")[:,100,100:]
-tvv, rvv = jnp.meshgrid(tv,yv[100:], indexing='ij')
+tv_select = np.where(tv>source_sd)[0]
+const_model_radial_data = jnp.load("data/c0_wavefield_line.npy")[tv_select,:] # avoid directly sampling origin
+tvv, rvv = jnp.meshgrid(tv[tv_select],yv[yv>=0], indexing='ij')
 drtvv = jnp.abs(tvv-rvv)/jnp.sqrt(2)
 rtscale = 1
 ryscale = 1
 tvv = tvv / rtscale
 rvv = rvv / rtscale
 dscale = jnp.std(const_model_radial_data)
+const_model_radial_data = const_model_radial_data / dscale
 
-dset_rng_key = jax.random.PRNGKey(1)
-rm_rng_key = jax.random.PRNGKey(2)
-num_batches = 100000
-batch_size = 2000
-radial_iterator = weighted_dataloader(dset_rng_key, 
-                                      jnp.reshape(const_model_radial_data, (-1,1)), # observed data has to go first, then coords in order used by model
-                                      jnp.reshape(tvv, (-1,1)), 
-                                      jnp.reshape(rvv, (-1,1)), 
-                                      num_batches = num_batches+1, 
-                                      batch_size = batch_size)
-dummy_t, dummy_r, dummy_u = next(radial_iterator)
 radial_model = default_mlp_model((64,64))
-init_radial_params = radial_model.init(rm_rng_key, jnp.hstack((dummy_t, dummy_r)))
 
-radial_schedule = optax.warmup_cosine_decay_schedule(
-  init_value=1e-3,
-  peak_value=1e-2,
-  warmup_steps=5000,
-  decay_steps=25_000,
-  end_value=1e-8,
-)
+if retrain_radial:
+    dset_rng_key = jax.random.PRNGKey(1)
+    rm_rng_key = jax.random.PRNGKey(2)
+    num_batches = 500_000
+    batch_size = 4096
+    radial_iterator = weighted_dataloader(dset_rng_key, 
+                                        jnp.reshape(const_model_radial_data, (-1,1)), # observed data has to go first, then coords in order used by model
+                                        jnp.reshape(tvv, (-1,1)), 
+                                        jnp.reshape(rvv, (-1,1)), 
+                                        num_batches = num_batches+1, 
+                                        batch_size = batch_size)
+    dummy_t, dummy_r, dummy_u = next(radial_iterator)
+    init_radial_params = radial_model.init(rm_rng_key, jnp.hstack((dummy_t, dummy_r)))
 
-radial_optimizer=optax.adabelief(learning_rate=radial_schedule)
+    radial_schedule = optax.warmup_cosine_decay_schedule(
+    init_value=1e-3,
+    peak_value=1e-2,
+    warmup_steps=50_000,
+    decay_steps=250_000,
+    end_value=1e-8,
+    )
 
-radial_params = train_to_data(init_radial_params, 
-                              Partial(radial_model_eval, radial_model, t1=t1, sd=source_sd),
-                              radial_optimizer, 
-                              u_loss_fn, 
-                              radial_iterator, 
-                              num_batches = num_batches)
+    radial_optimizer=optax.adamw(learning_rate=radial_schedule)
+
+    radial_params = train_to_data(init_radial_params, 
+                                Partial(radial_model_eval, radial_model, t1=t1, sd=source_sd),
+                                radial_optimizer, 
+                                u_loss_fn, 
+                                radial_iterator, 
+                                num_batches = num_batches)
+
+    with open("data/radial2d.pickle", 'wb') as file:
+        pickle.dump(radial_params, file)
+
+    print("Done training radial")
+
+else:
+    with open("data/radial2d.pickle", 'rb') as file:
+        radial_params = pickle.load(file)
 
 
+radial_evaluate = Partial(radial_model_eval, radial_model, radial_params, t1=t1, sd=source_sd)
+upredtest = jax.vmap(radial_evaluate)(tvv.reshape(-1,1), rvv.reshape(-1,1))
+fig, axes = plt.subplots(1,3)
+axes[0].contourf(rvv,tvv,jnp.clip(const_model_radial_data,-3,3))
+axes[1].contourf(rvv,tvv,jnp.clip(upredtest.reshape(rvv.shape),-3,3))
+axes[2].contourf(rvv,tvv,jnp.clip(const_model_radial_data-upredtest.reshape(rvv.shape),-1,1), cmap='seismic')
+fig.show()
 
-hbatch_size = 10000
+
+hbatch_size = 25000
 full_model = default_mlp_model((64,32,32,16), output=4, output_bias=False)
-fm_dset_rng_key = jax.random.PRNGKey(3)
-fm_rng_key = jax.random.PRNGKey(4)
-dummy_fm_coords, dummy_fm_weights = uniform_plus_kde_sampler(fm_dset_rng_key, 3, hbatch_size=hbatch_size)
-init_fm_params = full_model.init(fm_rng_key, jnp.hstack(dummy_fm_coords))
-init_fm_params= scale_param(init_fm_params, 0.0)
-fm_evaluate = Partial(model_eval_2d, radial_model, radial_params, full_model, t1=t1, sd=source_sd)
-fm_optimizer = optax.lion(learning_rate=1e-4)
-(anneal_params, epoch_loss_history, coords, colloc_weights) = train_to_physics(fm_rng_key, init_fm_params, fm_evaluate, fm_optimizer, phys_loss_fn_2d, uniform_plus_kde_sampler, jvgaussian_c, anneal_schedule=jnp.linspace(0.01,1,25), epochs=25)
+fm_evaluate = Partial(model_eval_2d, radial_model, radial_params, full_model, t1=t1, sd=2*source_sd)
 
-(anneal_params2, epoch_loss_history2, coords2, colloc_weights2) = train_to_physics(fm_rng_key, init_fm_params, fm_evaluate, fm_optimizer, phys_loss_fn_2d, uniform_plus_kde_sampler,jvgaussian_c, anneal_schedule=0.01*jnp.ones(1),epochs=1)
+if retrain_full:
+    fm_optimizer = optax.lion(learning_rate=1e-4)
+    fm_dset_rng_key = jax.random.PRNGKey(3)
+    fm_rng_key = jax.random.PRNGKey(4)
+    resampler = Partial(uniform_plus_kde_sampler, sd=2*source_sd, bandwidth=0.1)
+    dummy_fm_coords, dummy_fm_weights = resampler(fm_dset_rng_key, 3, hbatch_size=hbatch_size)
+    init_fm_params = full_model.init(fm_rng_key, jnp.hstack(dummy_fm_coords))
+    init_fm_params= scale_param(init_fm_params,1)
+    train_output = train_to_physics(fm_rng_key, init_fm_params, fm_evaluate, fm_optimizer, phys_loss_fn_2d, resampler, jvgaussian_c, anneal_schedule=jnp.hstack((jnp.linspace(0.01,1,100), jnp.ones(50))), epochs=150)
+    with open("data/hetero_train_output2d.pickle", 'wb') as file:
+        pickle.dump(train_output, file)
+
+    fm_optimizer = optax.lion(learning_rate=3e-4)
+    (anneal_params, epoch_loss_history, coords, colloc_weights) = train_output
+    fm_dset_rng_key = jax.random.PRNGKey(5)
+    fm_rng_key = jax.random.PRNGKey(6)
+    resampler = Partial(uniform_plus_kde_sampler, sd=2*source_sd, bandwidth=0.02)
+    train_output = train_to_physics(fm_rng_key, anneal_params, fm_evaluate, fm_optimizer, phys_loss_fn_2d, resampler, jvgaussian_c, anneal_schedule=np.ones(50), epochs=50)
+    with open("data/hetero_train_output2d_2.pickle", 'wb') as file:
+        pickle.dump(train_output, file)
+
+else:
+    with open("data/hetero_train_output2d_2.pickle", 'rb') as file:
+        train_output = pickle.load(file)
+
+(anneal_params, epoch_loss_history, coords, colloc_weights) = train_output
+
+# (anneal_params2, epoch_loss_history2, coords2, colloc_weights2) = train_to_physics(fm_rng_key, init_fm_params, fm_evaluate, fm_optimizer, phys_loss_fn_2d, resampler,jvgaussian_c, anneal_schedule=np.ones(100),epochs=100)
+
+gt_data = np.load("data/cg_wavefield.npy")
+xtest = xv[::10]
+ytest = yv[::10]
+ttest = tv[::10]
+xsel = np.where(np.abs(xtest)<=1)[0]
+ysel = np.where(np.abs(ytest)<=1)[0]
+xtest = xtest[xsel]
+ytest = ytest[ysel]
+xxtest, yytest = jnp.meshgrid(xtest, ytest, indexing="ij")
+c2 = jvgaussian_c(jnp.hstack((xxtest.reshape(-1,1), yytest.reshape(-1,1))), 1.0).reshape(-1,1)
+tep = Partial(fm_evaluate, anneal_params)
 
 
-c2 = jvgaussian_c(jnp.hstack(coords[1:]), 1.0).reshape(-1,1)
-loss, res = phys_loss_fn_2d(fm_evaluate, init_fm_params, c2, *coords, weights=colloc_weights)
+fig, axes = plt.subplots(4,4)
+testtimes = [0.1,0.25,0.50,0.75]
+
+for (i,testtime) in enumerate(testtimes): 
+    testidx = np.argmin(np.abs(ttest-testtime))
+    actualtesttime = ttest[testidx]
+    print(testidx, actualtesttime)
+    tttest = actualtesttime*jnp.ones(xxtest.shape)
+    gt_slice = gt_data[testidx,xsel[0]:xsel[-1]+1,ysel[0]:ysel[-1]+1]
+    upredtest = jax.vmap(tep)(tttest.reshape(-1,1), xxtest.reshape(-1,1), yytest.reshape(-1,1))
+    loss, res = phys_loss_fn_2d(fm_evaluate, anneal_params, c2, tttest.reshape(-1,1), xxtest.reshape(-1,1), yytest.reshape(-1,1))
+
+    axes[0,i].contourf(xxtest, yytest, gt_slice)
+    axes[1,i].contourf(xxtest, yytest, upredtest.reshape(xxtest.shape))
+    axes[2,i].contourf(xxtest, yytest, gt_slice - upredtest.reshape(xxtest.shape))
+    axes[3,i].contourf(xxtest, yytest, res.reshape(xxtest.shape))
+
+fig.show()
+
+# plt.figure(figsize=(4,3))
+# plt.contourf(xxtest, yytest, upredtest.reshape(101,101))
+# plt.title("PINN Solution", fontsize=14)
+# plt.colorbar(label="Amplitude (arb.)")
+# plt.show()
+
+# # plt.savefig("figures/pinn2D.pdf")
+# # plt.close()
+# c2 = jvgaussian_c(jnp.hstack(coords[1:]), 1.0).reshape(-1,1)
+# loss, res = phys_loss_fn_2d(fm_evaluate, init_fm_params, c2, *coords, weights=colloc_weights)
--- terrapinn/dataloaders.py
+++ terrapinn/dataloaders.py
@@ -4,12 +4,16 @@ from sklearn.neighbors import KernelDensity
 import warnings
 
 
-def weighted_dataloader(rng_key, *data, weights=None, num_batches=30000, batch_size=10000):
+def weighted_dataloader(rng_key, *data, weights=None, num_batches=30000, batch_size=10000, jitter=None):
     split_rng_key = jax.random.split(rng_key, num_batches)
     def _dset_generator():
         for i in range(num_batches):
             idx = jax.random.choice(split_rng_key[i], len(data[0]), (batch_size,))
-            yield [d[idx] for d in data]
+            if jitter is not None:
+                jkeys = jax.random.split(split_rng_key[i], len(jitter))
+                yield [d[idx]*(1+j*jax.random.normal(jk)) for (d,j,jk) in zip(data, jitter, jkeys)]
+            else:
+                yield [d[idx] for d in data]
     
     return _dset_generator()
 
@@ -22,7 +26,7 @@ def weighted_dataloader(rng_key, *data, weights=None, num_batches=30000, batch_s
 #     untransformed_resampled_coords = jnp.tanh(resampled_coords)
 #     return untransformed_resampled_coords
 
-def uniform_plus_kde_sampler(rng_key, ndims, weights=None, old_coords=None, hbatch_size=10000, bandwidth=0.5):
+def uniform_plus_kde_sampler(rng_key, ndims, weights=None, old_coords=None, hbatch_size=10000, bandwidth=0.5, sd=1.0):
     rng_key_1, rng_key_2 = jax.random.split(rng_key)
     sample_half_1 = jax.random.uniform(rng_key_1, (hbatch_size, ndims), minval=-1.0, maxval=1.0)
     if weights == None:
@@ -31,24 +35,24 @@ def uniform_plus_kde_sampler(rng_key, ndims, weights=None, old_coords=None, hbat
         sample_probability_weight = jnp.ones((2*hbatch_size, 1))
     else:
         kde = KernelDensity(kernel='tophat', bandwidth=bandwidth)
-        #transform old time coordinate from [0,1] to [-1,1]
+        #transform old time coordinate from [sd,1] to [-1,1]
         old_coords = jnp.hstack(old_coords)
-        old_coords = old_coords.at[:,0].multiply(2.0)
-        old_coords = old_coords.at[:,0].add(-1.0)
+        old_coords = old_coords.at[:,0].multiply(2.0+2*sd)
+        old_coords = old_coords.at[:,0].add(-1.0-2*sd)
         transformed_coords = jnp.arctanh(old_coords)
         transform_jacobian = jnp.prod(1/(1-jnp.square(old_coords)), axis=1)
         kde.fit(transformed_coords, sample_weight=weights/transform_jacobian) #divide by transform jacobian to get probability in transformed space
         resampled_coords = kde.sample(hbatch_size, random_state=int(rng_key_2[1]))
         sample_half_2 = jnp.tanh(resampled_coords)
         samples = jnp.vstack((sample_half_1, sample_half_2))
-        sample_log_probability = kde.score_samples(jnp.arctanh(samples)) - jnp.sum(jnp.log((1-jnp.square(samples))), axis=1) + jnp.log(2) # multiply by transform jacobian to get probability in original space, log(2) factor comes from transforming time from [0,1] to [-1,1]
+        sample_log_probability = kde.score_samples(jnp.arctanh(samples)) - jnp.sum(jnp.log((1-jnp.square(samples))), axis=1) + jnp.log(2+2*sd) # multiply by transform jacobian to get probability in original space, log(2) factor comes from transforming time from [sd,1] to [-1,1]
         #transform time coordinate from [-1,1] to [0,1]     
-        uniform_inverse_density = 2*(ndims-1)
+        uniform_inverse_density = 2*(ndims-1)*(1-sd)
         sample_probability_weight = 1/(0.5+0.5*uniform_inverse_density*jnp.exp(sample_log_probability)) # = uniform / (0.5*uniform + 0.5*resampling)
     
-    #transform time coordinate from [-1,1] to [0,1]
-    samples = samples.at[:,0].add(1.0)
-    samples = samples.at[:,0].divide(2.0)
+    #transform time coordinate from [-1,1] to [sd,1]
+    samples = samples.at[:,0].add(1.0+2*sd)
+    samples = samples.at[:,0].divide(2.0+2*sd)
     return ([s.reshape(-1,1) for s in samples.T], sample_probability_weight) # return samples, colloc_weights)
 
 
